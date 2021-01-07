58c0b1742459fffae8161c62f4da2d65665425d5
Novoselov2077 <Novoselov2077@mail.ru>
2021-01-07 21:08:51 +0300

    Add the first blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	"""docstring for Post"""
+	blog_title = models.CharField(max_length=300)
+	blog_date = models.DateTimeField()
+	blog_image = models.ImageField(upload_to='event_images/')
+	blog_text = models.TextField(max_length=1000)
--- events/models.py
+++ events/models.py
@@ -5,6 +5,6 @@ class Event(models.Model):
 	"""docstring for Event"""
 	event_image = models.ImageField(upload_to='event_images/')
 	event_text = models.CharField(max_length=300)
-	
+
 
 		
